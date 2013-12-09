c5c466d216abebde059df1db85582b166e1a2a0e
bsastre <bsastre@gmail.com>
2013-12-09 11:02:32 -0200

    redimensión de imágenes

--- admin/conectarse.php
+++ admin/conectarse.php
@@ -1,7 +1,7 @@
 <?php 
 function Conectarse() 
 { 
-   if (!($link=mysql_connect("localhost","root",""))) 
+   if (!($link=mysql_connect("bruno_server","root","FABO"))) 
   // if (!($link=mysql_connect("localhost","hiltonmo_admin","sU3ud2Er4b"))) 
    { 
       echo "Error conectando a la base de datos."; 
